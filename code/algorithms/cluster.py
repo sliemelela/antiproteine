0a4407a0f73aa74206ce68c634b7521b6d733bc2
Sara Morcy <sara0morcy@gmail.com>
2020-06-23 01:35:56 +0200

    Cleaned up random and random with swap algorithm into a class.

--- code/algorithms/cluster.py
+++ code/algorithms/cluster.py
@@ -2,13 +2,21 @@ from ..algorithms import annealing
 from ..classes import district as dt
 
 class Cluster(annealing.Annealing):
-    
+    """
+    Algorithm that repositions the batteries based on the existing clusters in a
+    given solution (k-means). The batteries are repositioned to the mean of the houses connected
+    to that battery in question.
+    This algorithm stops repositioning after the difference in price becomes (relatively) small.
+    The initial solution and new configurations of cables are made with the Greedy House Swap algorithm. 
+    """
+
     def reposition_batteries(self, solution):
         """
         Given some configuration of a district (solution), we calculate the mean position
         of the houses per battery. We reposition the battery to that location. 
         After this we run the Greedy House algorithm to find a configuration with the new location.
         """
+
         # Calculate the mean position of the houses (cluster) per battery
         new_battery_positions = []
         for battery in solution["district"].batteries:
@@ -32,10 +40,12 @@ class Cluster(annealing.Annealing):
                 battery.position = new_battery_positions[index_counter]
                 index_counter += 1
             
+            # Make a new configuration from the repositioned battery district
             new_solution = self.run_houses_swap()
             if new_solution["district"].total_cost <= solution["district"].total_cost:
                 break
 
+            # Stop if the amount of iterations is too high
             iterations += 1
             if iterations >= 100:
                 new_solution = solution
@@ -44,7 +54,11 @@ class Cluster(annealing.Annealing):
         return new_solution
     
     def run_cluster(self):
-        
+        """
+        Repeat the repositioning of batteries until the difference in price of the solution converges.
+        This is done by imposing a threshold (called epsilon) for which if the difference is smaller, we 
+        consider the last configuration as "converged".
+        """
         # Make initial solution
         init_solution = self.run_houses_swap()
 
